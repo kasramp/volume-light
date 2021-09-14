c35b0d6a818dc8b34a7a1f3d2bc003648c2f18e0
kasramp <kasra_mp@yahoo.com>
2021-09-14 22:46:05 +0200

    Add notification support

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from pulsectl import Pulse
-# gi.require_versions({"Gtk": "3.0", "Gdk": "3.0", "Keybinder": "3.0"})
-# from gi.repository import Gtk
+import gi
 
+gi.require_versions({'Notify': '0.7', 'Gio': '2.0', 'GLib': '2.0'})
+from pulsectl import Pulse
 from pynput import keyboard
+from gi.repository import Notify, GLib
 
 
 def main():
@@ -10,6 +11,11 @@ def main():
     volume_keys = ['media_volume_up', 'media_volume_down', 'media_volume_mute']
     devices = list(map(lambda sink: sink.description, pulse.sink_list()))
     print(devices)
+
+    Notify.init('volume-light')
+    notification = Notify.Notification.new('')
+    notification.set_timeout(Notify.EXPIRES_DEFAULT)
+    notification.set_hint("synchronous", GLib.Variant.new_strv("volume"))
 
     def on_press(key):
         try:
@@ -22,28 +28,30 @@ def main():
                     if increased_volume > sink.base_volume:
                         increased_volume = 1
                     pulse.volume_set_all_chans(sink, increased_volume)
+                    # notification
+                    notification.set_hint("value", GLib.Variant.new_int32(increased_volume * 100))
+                    notification.update("light-volume", None, "")
+                    notification.show()
                 if key.name == volume_keys[1]:
                     decreased_volume = sink.volume.value_flat - 0.02
                     if decreased_volume < 0:
                         decreased_volume = 0
                     pulse.volume_set_all_chans(sink, decreased_volume)
+                    # notification
+                    notification.set_hint("value", GLib.Variant.new_int32(decreased_volume * 100))
+                    notification.update("light-volume", None, "")
+                    notification.show()
                 if key.name == volume_keys[2]:
                     pulse.mute(sink, sink.mute == 0)
+                    notification.set_hint("value", None)
+                    notification.update("light-volume " + ("unmuted" if sink.mute == 0 else "muted"), None, "")
+                    notification.show()
 
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
     listener.join()
 
-    # def print_events(ev):
-    #     print('Pulse event:', ev)
-    #     # ev.t == 'remove' or ev.t == 'new' (update the profile)
-    #     ### Raise PulseLoopStop for event_listen() to return before timeout (if any)
-    #     # raise pulsectl.PulseLoopStop
-
-    # pulse.event_mask_set('all')
-    # pulse.event_callback_set(print_events)
-    # pulse.event_listen()
-    # timeout=100
+    Notify.uninit()
 
 
 if __name__ == '__main__':
