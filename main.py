b2e9841c6679e265abd25d6c2618afeba8439935
kasramp <kasra_mp@yahoo.com>
2021-09-16 19:16:59 +0200

    Update volume tray on keypress

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def main():
     volume_keys = ['media_volume_up', 'media_volume_down', 'media_volume_mute']
     devices = list(map(lambda sink: sink.description, pulse.sink_list()))
     print(devices)
-
     Notify.init('volume-light')
     notification = Notify.Notification.new('')
     notification.set_timeout(Notify.EXPIRES_DEFAULT)
@@ -19,17 +18,14 @@ def main():
 
     indicator = AppIndicator3.Indicator.new('volume-light', 'volume-light', AppIndicator3.IndicatorCategory.OTHER)
     indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-    indicator.set_icon('audio-volume-low')
 
-    ## StatusIcon is deprecated, better use Xapp by mint https://blog.linuxmint.com/?p=3795
-    ## Or stick to appIndicator
+    sink = pulse.get_sink_by_name(pulse.server_info().default_sink_name)
+
+    indicator.set_icon(get_volume_icon(sink.volume.value_flat*100, sink.mute != 0))
+
     # tray = Gtk.StatusIcon()
     # tray.set_from_icon_name("audio-volume-low")
     # tray.set_tooltip_text("Test")
-
-
-    ## The main blocks keyboard listener handler
-    Gtk.main()
 
     def on_press(key):
         try:
@@ -46,6 +42,7 @@ def main():
                     notification.set_hint("value", GLib.Variant.new_int32(increased_volume * 100))
                     notification.update("light-volume", None, "")
                     notification.show()
+                    indicator.set_icon(get_volume_icon(sink.volume.value_flat * 100, sink.mute != 0))
                 if key.name == volume_keys[1]:
                     decreased_volume = sink.volume.value_flat - 0.02
                     if decreased_volume < 0:
@@ -55,18 +52,36 @@ def main():
                     notification.set_hint("value", GLib.Variant.new_int32(decreased_volume * 100))
                     notification.update("light-volume", None, "")
                     notification.show()
+                    indicator.set_icon(get_volume_icon(sink.volume.value_flat * 100, sink.mute != 0))
                 if key.name == volume_keys[2]:
                     pulse.mute(sink, sink.mute == 0)
                     notification.set_hint("value", None)
                     notification.update("light-volume " + ("unmuted" if sink.mute == 0 else "muted"), None, "")
                     notification.show()
+                    indicator.set_icon(get_volume_icon(sink.volume.value_flat * 100, sink.mute != 0))
 
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
-    listener.join()
-
+    Gtk.main()
     Notify.uninit()
+
+
+def get_volume_icon(volume, is_muted):
+    if is_muted:
+        return 'audio-volume-muted'
+    if volume < 100.0 / 3:
+        icon_name = 'audio-volume-low'
+    elif volume < 100.0 / 3 * 2:
+        icon_name = 'audio-volume-medium'
+    else:
+        icon_name = 'audio-volume-high'
+    return icon_name
 
 
 if __name__ == '__main__':
     main()
+
+# docs:
+## file:///usr/share/gtk-doc/html/libnotify/index.html
+## http://lazka.github.io/pgi-docs/#Notify-0.7/classes/Notification.html#Notify.Notification.set_hint
+## http://lazka.github.io/pgi-docs/GLib-2.0/classes/Variant.html#GLib.Variant.new_string
